a6f7a54d86d925c7346b103690c879963776f8bd
RickHPotter <luisfla55@hotmail.com>
2024-06-26 14:51:42 -0300

    touch up request class

--- app/request.rb
+++ app/request.rb
@@ -2,31 +2,47 @@
 
 class Request
   attr_reader :request,
-              :request_line, :verb, :path, :http,
+              :request_line, :verb, :path, :paths, :params, :version,
               :headers, :host, :port, :user_agent, :accept,
               :body
 
   def initialize(request)
     @request = request.first
+    return if @request.nil?
+
     dissect_request
   end
 
   def dissect_request
-    @request_line, *headers_body = request.split("\r\n")
-    dissect_request_line
-    dissect_headers_body(headers_body)
+    altered_request, @body = request.split("\r\n\r\n")
+
+    @request_line, *array_of_headers = altered_request.split("\r\n")
+    @verb, @path, @version = request_line.split(" ")
+
+    dissect_headers(array_of_headers)
+    dissect_path
   end
 
-  def dissect_request_line
-    @verb, @path, @http = request_line.split(" ")
+  def dissect_headers(array_of_headers)
+    @full_host, array_of_headers  = array_of_headers.partition { |header| header.start_with?("Host: ") }
+    @user_agent, array_of_headers = array_of_headers.partition { |header| header.start_with?("User-Agent: ") }
+    @accept, leftovers            = array_of_headers.partition { |header| header.start_with?("Accept: ") }
+
+    @headers = {
+      host: @full_host.first.gsub("Host: ", ""),
+      user_agent: @user_agent.first.gsub("User-Agent: ", ""),
+      accept: @accept.first.gsub("Accept: ", "")
+    }
+    @headers.merge!(leftovers:) unless leftovers.empty?
+
+    @host, @port = @headers[:host].split(":")
+    @user_agent  = @headers[:user_agent]
+    @accept      = @headers[:accept]
   end
 
-  def dissect_headers_body(headers_body)
-    host_port, user_agent, media_types_to_accept, *body = headers_body
-    @headers = [host_port, user_agent, media_types_to_accept]
-    @host, @port = host_port&.gsub("Host: ", "")&.split(":")
-    @user_agent = user_agent&.gsub("User-Agent: ", "")
-    @accept = media_types_to_accept&.gsub("Accept: ", "")
-    @body = body
+  def dissect_path
+    @path = @path[1..] if @path.start_with?("/")
+    @path, @params = @path.split("?")
+    @paths = @path.split("/")
   end
 end
